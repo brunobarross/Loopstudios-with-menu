501ff3fc0e1072b181fe9f4c0b9c43f4319b6a52
Altamiro Bruno <62890135+brunobarross@users.noreply.github.com>
2021-04-12 13:52:48 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,12 +3,29 @@ function menuMobile() {
     let inptMenu = document.getElementById('inptMenu');
     let menu =  document.getElementById('menu-mobile');
     let containerMenu = document.getElementById('nav-menu');
+    let intro = document.getElementById('intro');
+
 
     if(inptMenu.checked == true && tamanhoTela < 739) {
       menu.style.display ="flex";
-      containerMenu.style.background="#000"
+      menu.classList.add('animaDiv');
+      menu.classList.remove('sobeDiv');
+      intro.style.marginTop ="150px";
+      intro.style.transition="0.5s";
+      inptMenu.style.transform = "rotate(90deg)";
+      inptMenu.style.transition = "0.5s";
+
     } else if (inptMenu.checked == false && tamanhoTela < 739) {
-      menu.style.display ="none";
+      menu.style.display="none";
+      menu.classList.remove('animaDiv');
+      intro.style.marginTop ="";
+      inptMenu.style.transform = "rotate(0)";
+    } else if(tamanhoTela > 739) {
+      menu.style.display="flex";
+      containerMenu.style.background="none";
+      
+
     }
+
 
 }
